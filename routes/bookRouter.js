6854637c8a247acd399ed3bb103c91a4c06ca498
Rizwan Ahmed <rizwanahmedziai@gmail.com>
2020-01-30 12:36:16 +0500

    Added routes

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -2,9 +2,7 @@ const express = require('express');
 
 function routes(Book) {
   const bookRouter = express.Router();
-
-  bookRouter
-    .route('/books')
+  bookRouter.route('/books')
     .post((req, res) => {
       const book = new Book(req.body);
 
@@ -22,20 +20,17 @@ function routes(Book) {
         }
         return res.json(books);
       });
-      // const response = { hello: 'This is my API' };
-      // res.json(response);
+    });
+  bookRouter.route('/books/:bookId')
+    .get((req, res) => {
+      Book.findById(req.params.bookId, (err, book) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
     });
 
-  bookRouter.route('/books/:bookId').get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(book);
-    });
-    // const response = { hello: 'This is my API' };
-    // res.json(response);
-  });
   return bookRouter;
 }
 
